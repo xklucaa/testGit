1c837602a4fda87f71437b2b3defd90055dab2e7
luca <xiongkun778@163.com>
2024-06-11 23:40:05 +0800

    create repository and local branch

---
+++ src/main/java/sg/nus/iss/javaspring/testgit/testPoint.java
@@ -0,0 +1,11 @@
+package sg.nus.iss.javaspring.testgit;
+
+public class testPoint {
+    //1.建立github上的仓库 ==》 copy http link ==》 start a new project ==》 创建一个repository ==>
+    //可以在下面的git界面选择文件ignore 就不会将这个更新到git ==》 然后再commit（此时这个操作只会再本地进行，而不会推送到github的远程库）
+    //最后push到远程库即可
+    //
+    //2.创建分支  ==》 在我们完成了本地项目跟远程项目的一个关联之后，我们可能就会需要进入到一个开发，在开发的过程中，
+    //我们一般不会在master这个分支上面去做开发，master这个分支一般只是用来接收别的分支合并过来的内容，所以我们需要新建一个自己的branch（local）
+    //
+}
