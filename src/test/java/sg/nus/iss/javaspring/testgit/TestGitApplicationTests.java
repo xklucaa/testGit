6f81f2fb44de2bc3b6cc96f51031b65b3e3ffbbb
luca <xiongkun778@163.com>
2024-06-11 23:29:47 +0800

    Init

---
+++ src/test/java/sg/nus/iss/javaspring/testgit/TestGitApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.javaspring.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
